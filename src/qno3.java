facb8214a2ad0b792ae3f85d76dd313d7de272e1
Kritan Niraula <kritanniraula@gmail.com>
2026-01-28 16:32:08 +0545

    qno 3 done

--- src/qno3.java
+++ src/qno3.java
@@ -1,23 +1,29 @@
-/*qno 1= introduction of OOPS concepts
+/*qno 1= introduction to constructor in OOPS concepts
 Date : 1/28/2026
 Author : Kritan Niraula */
-public class qno2 {
+public class qno3 {
     public static void main(String[] args) {
-        Student s1 = new Student();
+         Student s1 = new Student();
         s1.name ="kritan";
         s1.age = 20;
-        s1.printname();
         s1.printinfo();
+
+        
     }
 }
 class Student{
     String name;
     int age;
 
+    Student(){
+            System.out.println("constructor called");
+        }
+
    public void printinfo(){
-        System.out.println(this.age);
-    }
-    public void printname(){
         System.out.println(this.name);
-    }
+        System.out.println(this.age);
+   }
+
+   
+    
 }
