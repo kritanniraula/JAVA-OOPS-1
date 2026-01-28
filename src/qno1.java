a8e38ec15fb56a8bcf8852e9f7184f1ddb10c051
Kritan Niraula <kritanniraula@gmail.com>
2026-01-28 13:55:45 +0545

    modified qno1

--- src/qno1.java
+++ src/qno1.java
@@ -6,11 +6,19 @@ public class qno1{
     public static void main(String[] args) {
         pen p1 = new pen();
         p1.color = "red";
-        p1.type = "ball";
+        p1.type = "ball point";
         p1.write();
         p1.printcolor();
         p1.printdetail();
         p1.printtype();
+
+        pen p2 = new pen();
+        p2.color = "black";
+        p2.type = "gel";
+        p2.write();
+        p2.printcolor();
+        p2.printdetail();
+        p2.printtype();
     }
 }
 class pen{
